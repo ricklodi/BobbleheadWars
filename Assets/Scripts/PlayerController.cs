95559d409e0126942f58f6e7b13a95cd98af4a15
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-09-28 18:36:43 -0400

    Chapter 4 - Raycasting - Completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody head;
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +28,39 @@ public class PlayerController : MonoBehaviour
         //pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;  //The Input.GetAxis returns only 1 or -1
         //pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
         //transform.position = pos;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+            0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask,
+            QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+            // 1
+            Vector3 targetPosition = new Vector3(hit.point.x,
+            transform.position.y, hit.point.z);
+            // 2
+            Quaternion rotation = Quaternion.LookRotation(targetPosition -
+            transform.position);
+            // 3
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+            rotation, Time.deltaTime * 10.0f);
+        }
     }
 }
