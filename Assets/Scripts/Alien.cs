6cb81ef4b193ddcf803f79db13c027e09fd3d4be
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-09-29 17:27:32 -0400

    Chapter 5 - Pathfinding - Completed

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour
+{
+    public Transform target;
+    public float navigationUpdate;
+    private float navigationTime = 0;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+    void Update()
+    {
+        if (target != null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
+        }
+    }
+}
