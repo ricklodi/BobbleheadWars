750a245d10f5ade9762f499cdb704f47ec661732
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-10-07 10:37:21 -0400

    Chapter 6 - Chapter Completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
+    public Animator bodyAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,11 +36,12 @@ public class PlayerController : MonoBehaviour
             0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+            bodyAnimator.SetBool("IsMoving", true);
         }
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
