1ffa47bb3d166429046c60eae031e19570741d2f
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-10-13 12:06:27 -0400

    Chapter 8 - Hero death and head removal

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -53,6 +53,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (player == null)
+        {
+            return;
+        }
         currentSpawnTime += Time.deltaTime;
         if (currentSpawnTime > generatedSpawnTime)
         {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,15 @@ public class PlayerController : MonoBehaviour
     public LayerMask layerMask;
     private Vector3 currentLookTarget = Vector3.zero;
     public Animator bodyAnimator;
+    
+    public float[] hitForce;
+    public float timeBetweenHits = 2.5f;
+    private bool isHit = false;
+    private float timeSinceHit = 0;
+    private int hitNumber = -1;
+
+    public Rigidbody marineBody;
+    private bool isDead = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +38,15 @@ public class PlayerController : MonoBehaviour
         //pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;  //The Input.GetAxis returns only 1 or -1
         //pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
         //transform.position = pos;
+        if (isHit)
+        {
+            timeSinceHit += Time.deltaTime;
+            if (timeSinceHit > timeBetweenHits)
+            {
+                isHit = false;
+                timeSinceHit = 0;
+            }
+        }
     }
     private void FixedUpdate()
     {
@@ -64,5 +82,44 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation,
             rotation, Time.deltaTime * 10.0f);
         }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Alien alien = other.gameObject.GetComponent<Alien>();
+        if (alien != null)
+        { // 1
+            if (!isHit)
+            {
+                hitNumber += 1; // 2
+                CameraShake cameraShake = Camera.main.GetComponent<CameraShake>();
+                if (hitNumber < hitForce.Length) // 3
+                {
+                    cameraShake.intensity = hitForce[hitNumber];
+                    cameraShake.Shake();
+                }
+                else
+                {
+                    Die();
+                }
+                isHit = true; // 4
+                SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
+            }
+            alien.Die();
+        }
+    }
+    public void Die()
+    {
+        bodyAnimator.SetBool("IsMoving", false);
+        marineBody.transform.parent = null;
+        marineBody.isKinematic = false;
+        marineBody.useGravity = true;
+        marineBody.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        marineBody.gameObject.GetComponent<Gun>().enabled = false;
+        //Getting rid of the head
+        Destroy(head.gameObject.GetComponent<HingeJoint>());
+        head.transform.parent = null;
+        head.useGravity = true;
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.marineDeath);
+        Destroy(gameObject);
     }
 }
